de3498f99cf3bb4a4c6fee7e29d193888e812a5e
samsonhaile28 <shaile@nevada.unr.edu>
2016-10-25 13:32:17 -0700

    Added base work done on simulation(Code can interpret location of robot in sim)

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/samson/catkin_ws/src
+# Build directory: /home/samson/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(location_monitor)
---
+++ catkin_ws/build/location_monitor/CMakeFiles/location_monitor_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/location_monitor_node.dir/src/location_monitor.cpp.o"
+  "/home/samson/catkin_ws/devel/lib/location_monitor/location_monitor_node.pdb"
+  "/home/samson/catkin_ws/devel/lib/location_monitor/location_monitor_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/location_monitor_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
